703e179e3d825aa5bb67ad45cc1a654f960c085a
kvbwork <kvbdev@yandex.ru>
2022-12-18 15:22:03 +0500

    refactor getAuthority(User) request

--- src/main/java/ru/netology/rest/controller/AuthorizationController.java
+++ src/main/java/ru/netology/rest/controller/AuthorizationController.java
@@ -1,14 +1,17 @@
 package ru.netology.rest.controller;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.netology.rest.enums.Authorities;
+import ru.netology.rest.model.User;
 import ru.netology.rest.service.AuthorizationService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@Validated
 public class AuthorizationController {
     private final AuthorizationService service;
 
@@ -17,7 +20,7 @@ public class AuthorizationController {
     }
 
     @GetMapping("/authorize")
-    public List<Authorities> getAuthorities(@RequestParam("user") String user, @RequestParam("password") String password) {
-        return service.getAuthorities(user, password);
+    public List<Authorities> getAuthorities(@Valid User user) {
+        return service.getAuthorities(user);
     }
 }
--- src/main/java/ru/netology/rest/service/AuthorizationService.java
+++ src/main/java/ru/netology/rest/service/AuthorizationService.java
@@ -1,9 +1,9 @@
 package ru.netology.rest.service;
 
 import org.springframework.stereotype.Service;
-import ru.netology.rest.exception.InvalidCredentials;
-import ru.netology.rest.exception.UnauthorizedUser;
 import ru.netology.rest.enums.Authorities;
+import ru.netology.rest.exception.UnauthorizedUser;
+import ru.netology.rest.model.User;
 import ru.netology.rest.repository.UserRepository;
 
 import java.util.List;
@@ -16,19 +16,12 @@ public class AuthorizationService {
         this.userRepository = userRepository;
     }
 
-    public List<Authorities> getAuthorities(String user, String password) {
-        if (isEmpty(user) || isEmpty(password)) {
-            throw new InvalidCredentials("User name or password is empty");
-        }
-        List<Authorities> userAuthorities = userRepository.getUserAuthorities(user, password);
+    public List<Authorities> getAuthorities(User user) {
+        List<Authorities> userAuthorities = userRepository.getUserAuthorities(user.getName(), user.getPassword());
         if (isEmpty(userAuthorities)) {
-            throw new UnauthorizedUser("Unknown user " + user);
+            throw new UnauthorizedUser("Unknown user " + user.getName());
         }
         return userAuthorities;
-    }
-
-    private boolean isEmpty(String str) {
-        return str == null || str.isEmpty();
     }
 
     private boolean isEmpty(List<?> str) {
